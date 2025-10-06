d5020bf53cc7947063ef4a37061a09f720600a04
antonioabadpro <antonioabadpro@gmail.com>
2025-10-06 17:20:17 +0200

    Realizamos los tests de la Clase 'Calculadora'

--- src/sesion3/Calculadora.java
+++ src/sesion3/Calculadora.java
@@ -5,22 +5,23 @@ public class Calculadora
 	
 	public static int suma(int n1, int n2)
 	{
-		return 5;
+		return n1+n2;
 	}
 	
 	public static int resta(int n1, int n2)
 	{
-		return 2;
+		return n1-n2;
 	}
 	
 	public static int multiplicacion(int n1, int n2)
 	{
-		return 6;
+		return n1*n2;
 	}
 	
 	public static int division(int n1, int n2)
 	{
-		return 3;
+		if(n2==0) return -1;
+		else return n1/n2;
 	}
 	
 	public static void main(String[] args)
