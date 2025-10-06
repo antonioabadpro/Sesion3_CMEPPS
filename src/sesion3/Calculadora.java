e8295ff9336d7001aba3c7a886095f6a5be836e4
antonioabadpro <antonioabadpro@gmail.com>
2025-10-06 17:16:01 +0200

    Creamos la clase 'Calculadora' y comprobamos si los tests funcionan

---
+++ src/sesion3/Calculadora.java
@@ -0,0 +1,30 @@
+package sesion3;
+
+public class Calculadora
+{
+	
+	public static int suma()
+	{
+		return 5;
+	}
+	
+	public static int resta()
+	{
+		return 2;
+	}
+	
+	public static int multiplicacion()
+	{
+		return 6;
+	}
+	
+	public static int division()
+	{
+		return 3;
+	}
+	
+	public static void main(String[] args)
+	{
+
+	}
+}
