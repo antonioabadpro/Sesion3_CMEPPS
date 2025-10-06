8ef3eb0e121597f7a1be3de4cb6b2afb3a8e654c
antonioabadpro <antonioabadpro@gmail.com>
2025-10-06 17:19:54 +0200

    Realizamos la refactorizacion de la Clase Calculadora

--- src/sesion3/Calculadora.java
+++ src/sesion3/Calculadora.java
@@ -3,22 +3,22 @@ package sesion3;
 public class Calculadora
 {
 	
-	public static int suma()
+	public static int suma(int n1, int n2)
 	{
 		return 5;
 	}
 	
-	public static int resta()
+	public static int resta(int n1, int n2)
 	{
 		return 2;
 	}
 	
-	public static int multiplicacion()
+	public static int multiplicacion(int n1, int n2)
 	{
 		return 6;
 	}
 	
-	public static int division()
+	public static int division(int n1, int n2)
 	{
 		return 3;
 	}
